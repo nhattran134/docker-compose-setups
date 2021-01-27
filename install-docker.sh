a30e69788341f44eb722fccfa4896a8157b01163
Ubuntu <nathan@Devops01.wtjiznhqda5uxpjqamcffsw5id.ix.internal.cloudapp.net>
2021-01-27 09:54:37 +0000

    add bash file

---
+++ install-docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo mkdir /srv/nexus
+sudo chmod 600 /srv/nexus
+
+export GITLAB_HOME=/srv/gitlab
+
+sudo sysctl -w vm.max_map_count=262144
+sudo echo "vm.max_map_count = 262144" >> /etc/sysctl.conf
+
